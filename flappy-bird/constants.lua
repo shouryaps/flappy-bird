cec5d91ae7cedf4e626bdba79f3245b622a91649
shouryaps <shourya4@gmail.com>
2022-10-02 23:42:47 +0530

    Added base scrolling

---
+++ flappy-bird/constants.lua
@@ -0,0 +1,14 @@
+WINDOW_WIDTH = 288
+WINDOW_HEIGHT = 512
+
+VIRTUAL_WIDTH = 288
+VIRTUAL_HEIGHT = 512
+
+-- background
+DAY_BACKGROUND_PATH = "resources/background-day.png"
+
+-- base
+BASE_PATH = "resources/base.png"
+BASE_HEIGHT = 112
+BASE_SCROLL_SPEED = 60
+BASE_LOOP_POINT = 336 - VIRTUAL_WIDTH -- 336 is the width of base image
---
+++ flappy-bird/main.lua
@@ -0,0 +1,43 @@
+local push = require 'utils/push'
+
+require('constants')
+
+local background = love.graphics.newImage(DAY_BACKGROUND_PATH)
+local base = love.graphics.newImage(BASE_PATH)
+local baseScroll = 0
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle('Flappy Bird')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.update(dt)
+    baseScroll = (baseScroll + BASE_SCROLL_SPEED * dt) % BASE_LOOP_POINT
+end
+
+function love.draw()
+    push:start()
+
+    -- draw background
+    love.graphics.draw(background, 0, 0)
+
+    -- draw base in bottom of screen
+    love.graphics.draw(base, -baseScroll, VIRTUAL_HEIGHT - BASE_HEIGHT)
+
+    push:finish()
+end
